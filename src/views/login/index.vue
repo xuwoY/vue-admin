ee78db9fff81f7a8b175d5122708e6e709301c4a
xuwoY <13333084654@163.com>
2020-02-27 17:04:13 +0800

    构建项目

---
+++ src/views/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="login">666</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background: #344a5f;
+}
+</style>
